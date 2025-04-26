81b9d0bcf16763fdd511fa0624a4b15cd3cd43dc
KAMAL15402 <rawatkamal1542002.com>
2025-04-26 13:50:58 +0530

    Stable project after Docker setup - core functionality complete

--- employeedb/settings.py
+++ employeedb/settings.py
@@ -1,13 +1,12 @@
+import environ
 import os
 from pathlib import Path
-import environ
-
-
-env = environ.Env()
-environ.Env.read_env()
-
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 
 SECRET_KEY = env('SECRET_KEY')
